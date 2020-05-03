2432e26d85173444526829a65db3e27fa3c8b29a
PiotrG25 <piotrgoralewicz25@gmail.com>
2020-05-03 10:57:17 +0200

    PQ 2.0

--- Zadanie Projektowe 2/PQ.h
+++ Zadanie Projektowe 2/PQ.h
@@ -16,23 +16,27 @@ class PQ {
 	Edge** arr; // the array
 
 
+	// methods of restoring heap invariant
 	void bubbleUp(int index);
 	void bubbleDown(int index);
 
+	// methods of calculating indexes of relatives
 	int parentIndexOf(int index);
 	int leftChildIndexOf(int index);
 	int rightChildIndexOf(int index);
 
 public:
-
-
+	
+	// creates PQ of fixed size
 	PQ(int size);
-	PQ(Edge** edges, int size);
 	~PQ();
 
+	// adds pointer to the edge to the PQ
 	void enqueue(Edge* e);
+	// removes pointer form the PQ and returns it
 	Edge* dequeue();
 
+	// returns number of elements in the PQ
 	int getSize();
 };
 
