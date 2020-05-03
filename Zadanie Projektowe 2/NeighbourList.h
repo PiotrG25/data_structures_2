0c293f89a8d0e9ed8cdbcfb73073bb4f69b54e5d
PiotrG25 <piotrgoralewicz25@gmail.com>
2020-05-03 10:23:54 +0200

    NeighbourList 2.0

--- Zadanie Projektowe 2/NeighbourList.h
+++ Zadanie Projektowe 2/NeighbourList.h
@@ -26,6 +26,11 @@ class NeighbourList {
 
 	NeighbourList();
 	NeighbourList(int from);
+
+	// creates new Element with pionter to Edge e
+	// !!! does not copy edge !!!
+	// PS: used only in static lists constructor
+	void add(Edge* e);
 public:
 	// deletes all Elements and Edges within these elements
 	~NeighbourList();
@@ -33,16 +38,16 @@ public:
 	// Makes Neighbour Lists from copies of edges in the array
 	static NeighbourList** makeNeighbourLists(Edge** arr, int edges, int vertices);
 
-	// creates new Element with pionter to Edge e
-	// !!! does not copy edge !!!
-	void add(Edge* e);
-
 	// returns pointer to array of edges copied from the list
 	// sets size to size of the array
 	Edge** getEdges(int& size);
 };
 
-
+// prints for each list 
+// list of edges in format:
+// i (v1, w1), (v2, w2), ...
+// where i is index of from vertex
+// vj and wj are index of neighbouring vertex and weight of egdge between
 void printNeighbourLists(NeighbourList** lists, int vertices);
 
 #endif
